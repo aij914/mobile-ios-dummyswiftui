af09c92580aeadefd278ede4ea42aaae25eba230
Kiu Ai <aij914@gmail.com>
2021-01-12 14:44:44 +0800

    layout basic UI elements.

--- dummy_swiftui/ContentView.swift
+++ dummy_swiftui/ContentView.swift
@@ -8,9 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let buttons = [
+        ["7", "8", "9", "X"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "="],
+        ["0", ".", ".", "X"]
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ForEach(buttons, id: \.self) { row in
+                HStack {
+                    ForEach(row, id: \.self) { button in
+                        Text(button)
+                            .font(.system(size: 32))
+                            .frame(width: 80, height: 80, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    }
+                }
+            }
+        }
     }
 }
 
