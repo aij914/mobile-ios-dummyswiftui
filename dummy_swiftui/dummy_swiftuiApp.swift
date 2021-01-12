dd67f044842f0b1b7b0bab70ea966bc06ffa8cff
Kiu Ai <aij914@gmail.com>
2021-01-12 14:14:17 +0800

    init commit.

---
+++ dummy_swiftui/dummy_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  dummy_swiftuiApp.swift
+//  dummy_swiftui
+//
+//  Created by Kiu Ai on 1/12/21.
+//
+
+import SwiftUI
+
+@main
+struct dummy_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
