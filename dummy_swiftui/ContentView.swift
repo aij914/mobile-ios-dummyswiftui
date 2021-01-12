dccb8bb5cddc2ed412b48c70c229f66b3c5b666f
Kiu Ai <aij914@gmail.com>
2021-01-12 15:50:13 +0800

    use enum button instead.

--- dummy_swiftui/ContentView.swift
+++ dummy_swiftui/ContentView.swift
@@ -9,11 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let buttons = [
-        ["7", "8", "9", "X"],
-        ["4", "5", "6", "-"],
-        ["1", "2", "3", "="],
-        ["0", ".", ".", "X"]
+    enum CalculatorButton: String {
+        case zero = "0", one = "1", two = "2", three = "3", four = "4", five = "5", six = "6", seven = "7", eight = "8", nine = "9"
+        
+        case equls = "=", plus = "+", minus = "-", multiply = "X", divide = "/"
+        case ac = "AC", plusMinus = "+/-", percent = "%"
+        
+        var backgroundColor: Color {
+            switch self {
+            case .zero, .one, .two, .three, .four, .five, .six, .seven, .eight, .nine:
+                return Color(.darkGray)
+            case .ac, .plusMinus, .percent:
+                return Color(.lightGray)
+            default:
+                return .orange
+            }
+        }
+    }
+    
+    let calculatorButtons: [[CalculatorButton]] = [
+        [.ac, .plusMinus, .percent, .divide],
+        [.four, .five, .six, .minus],
+        [.seven, .eight, .nine, .multiply],
+        [.four, .five, .six, .minus],
+        [.one, .two, .three, .plus],
     ]
     
     var body: some View {
@@ -27,14 +46,14 @@ struct ContentView: View {
                         .font(.system(size: 64))
                 }.padding()
                 
-                ForEach(buttons, id: \.self) { row in
+                ForEach(calculatorButtons, id: \.self) { row in
                     HStack (alignment: .center, spacing: 12, content: {
                         ForEach(row, id: \.self) { button in
-                            Text(button)
+                            Text(button.rawValue)
                                 .font(.system(size: 32))
                                 .frame(width: buttonWidth(), height: self.buttonWidth(), alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                                 .foregroundColor(.white)
-                                .background(Color.yellow)
+                                .background(button.backgroundColor)
                                 .cornerRadius(buttonCornerRadius())
                         }
                     })
@@ -54,6 +73,10 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+            ContentView()
+        }
     }
 }
