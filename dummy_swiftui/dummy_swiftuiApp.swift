c7d27ba38d81ce31923d3b7cce2f1e3cdbb981ab
Kiu Ai <aij914@gmail.com>
2021-02-21 09:36:24 +0800

    fix for: missing env object crash when run on simulator.

--- dummy_swiftui/dummy_swiftuiApp.swift
+++ dummy_swiftui/dummy_swiftuiApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct dummy_swiftuiApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().environmentObject(GlobalEnvironment())
         }
     }
 }
